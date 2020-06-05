7e656ac09f84673d7ef0a2cf513d835219f3a3fe
Fly me to the moon <git@bigly.dog>
2020-06-04 22:37:21 -0700

    tpm

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -eu
 set -o pipefail
 
 
-plugins="$(rg '^set -g @plugin' < entry.conf | awk 'NR>1{print $0}')"
+plugins="$(rg '^set -g @plugin' < entry.conf)"
 uris="$(echo "$plugins" | sd 'set -g @plugin "([^"]+)"' '$1' | xargs -l printf 'https://github.com/%s\n')"
 
 cd "$(dirname "$0")/plugins" || exit 1
