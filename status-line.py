4cb3cdf291e02523037720fdbb6248f501fd0f66
Fly me to the moon <git@bigly.dog>
2021-09-30 14:47:03 -0700

    better code

--- status-line.py
+++ status-line.py
@@ -101,14 +101,9 @@ def _snap() -> _Snapshot:
 
 
 def _states() -> Tuple[_Snapshot, _Snapshot]:
-    span = 1
-    if s1 := _load():
-        sleep(max(0, span - (time() - s1.time)))
-        s2 = _snap()
-    else:
-        s1 = _snap()
-        sleep(max(0, span - (time() - s1.time)))
-        s2 = _snap()
+    s1 = _load() or _snap()
+    sleep(max(0, 1 - (time() - s1.time)))
+    s2 = _snap()
 
     json = dumps(asdict(s2), check_circular=False, ensure_ascii=False)
     _dump(_SNAPSHOT, thing=json)
